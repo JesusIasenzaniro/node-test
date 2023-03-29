89cd34fc9f1392ed2a03d0a1964de751728ee55c
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-03-29 14:52:32 +0200

    add basic routes

---
+++ routes/characters.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.send('<h1>Hello World</h1>');
+});
+
+router.get('/characters', (req, res, next) => {
+    res.send('<h1>Characters</h1>');
+});
+
+module.exports = router;
