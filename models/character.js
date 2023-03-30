3167546aca2b0b31a4734eab3b44feeca3ccf614
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-03-30 16:48:27 +0200

    refactor character schema

--- models/character.js
+++ models/character.js
@@ -3,119 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const characterSchema = new Schema({
-    id: {
-        type: Number,
-        required: true,
-        unique: true,
-    },
     name: {
         type: String,
         required: true,
     },
     description: {
         type: String,
-        required: true,
         default: '',
-    },
-    modified: {
-        type: Date,
-        required: true,
-    },
-    thumbnail: {
-        path: {
-            type: String,
-            required: true,
-        },
-        extension: {
-            type: String,
-            required: true,
-        },
-    },
-    resourceURI: {
-        type: String,
-        required: true,
-    },
-    comics: {
-        available: {
-            type: Number,
-            required: true,
-        },
-        collectionURI: {
-            type: String,
-            required: true,
-        },
-        items: [
-            {
-                resourceURI: {
-                    type: String,
-                    required: true,
-                },
-                name: {
-                    type: String,
-                    required: true,
-                },
-            },
-        ],
-        returned: {
-            type: Number,
-            required: true,
-        },
-    },
-    series: {
-        available: {
-            type: Number,
-            required: true,
-        },
-        collectionURI: {
-            type: String,
-            required: true,
-        },
-        items: [
-            {
-                resourceURI: {
-                    type: String,
-                    required: true,
-                },
-                name: {
-                    type: String,
-                    required: true,
-                },
-            },
-        ],
-        returned: {
-            type: Number,
-            required: true,
-        },
-    },
-    stories: {
-        available: {
-            type: Number,
-            required: true,
-        },
-        collectionURI: {
-            type: String,
-            required: true,
-        },
-        items: [
-            {
-                resourceURI: {
-                    type: String,
-                    required: true,
-                },
-                name: {
-                    type: String,
-                    required: true,
-                },
-                type: {
-                    type: String,
-                    required: true,
-                },
-            },
-        ],
-        returned: {
-            type: Number,
-            required: true,
-        },
     },
 });
 
